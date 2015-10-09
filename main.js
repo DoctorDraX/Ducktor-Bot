d6908c248cfd92b2ddf3af46366b8de38108eed4
megaslowbro1 <megaslowbro1@gmail.com>
2015-10-08 23:18:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -181,7 +181,7 @@ if (config.url) {
 				}
 				// The rooms that should be joined.
 				//autojoin code
-				try {autjoin/heatahfajita
+				try {autjoin/HeatahFajita
 					config.rooms = JSON.parse(fs.readFileSync('data/newrooms/' + config.nick + '_' + config.serverid + '.json'));;
 				}
 				catch (e) {
